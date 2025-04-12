51e3b2b9aa9cd4a7286b29a4e399b0b1979d20e9
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-04-12 15:33:45 +0200

    feat: implement email sending functionality in Contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,25 @@
 "use client"
 import { Button } from "@/components/ui/button";
+import { FormEvent, useRef, useState } from "react"
+import emailjs from "@emailjs/browser"
 
 export default function Contact(){
+    const [loading,set_loading] = useState<boolean>(false)
+    const form: any = useRef(null)
+    const send_application = (e: FormEvent) => {
+        e.preventDefault()
+        set_loading(true)
+        emailjs.sendForm("service_xwf6fms", "template_c4pjfnx", form.current, {
+            publicKey: "CLt7ZGjfiSQEopImB"
+        }).then(() => {
+            alert("✅ We received your enquiry, expect a call or an email soon!")
+            form.current.reset()
+        }).catch(() => {
+            alert("⚠️ Message not sent, please try again or send us a message on our email support@aurorasystems.co.zw!")
+        }).finally(()=>{
+            set_loading(false)
+        })
+    }
     return(
         <div>
             <div className=" bg-black text-white p-10">
@@ -15,29 +33,29 @@ export default function Contact(){
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3012.3124891263137!2d30.972765148432288!3d-17.87718512574054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1931a6ad51d8722b%3A0x56560381d7718065!2sGleneagles!5e1!3m2!1sen!2szw!4v1744293348727!5m2!1sen!2szw " className="w-full h-100 rounded"  loading="lazy" ></iframe>
                         </div>
                         <div className=" w-full px-2">
-                            <form action="">
+                            <form ref={form} onSubmit={send_application} >
                                 <div>
                                     <h4>Leave Us A Message</h4>
                                     <br/>
                                 </div>
                                 <div className="mb-3">
                                     <span>Full Name</span>
-                                    <input className="form-control" type="" name="" required/>
+                                    <input className="form-control" type="text" name="name" required/>
                                 </div>
                                 <div className="mb-3">
                                     <span>Email</span>
-                                    <input className="form-control" type="" name="" required/>
+                                    <input className="form-control" type="email" name="email" required/>
                                 </div>
                                 <div className="mb-3">
                                     <span>Contact Number</span>
-                                    <input className="form-control" type="" name="" required/>
+                                    <input className="form-control" type="contact_number" name="contact_number" required/>
                                 </div>
                                 <div className="mb-3">
                                     <span>Message</span>
-                                    <textarea className="form-control" name="" id="" required></textarea>
+                                    <textarea className="form-control" name="message" required></textarea>
                                 </div>
                                 <div className="mb-3">
-                                <Button size="lg" className="bg-red-600 hover:bg-red-700 text-white">
+                                <Button type="submit" size="lg" className="bg-red-600 hover:bg-red-700 text-white">
                 Send
             </Button>
                                 </div>
