c2f18ca54760adfe0e831d7589e932e5734e8c0f
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2025-04-24 11:05:09 +0200

    feat: add multiple images to Gallery and enable thumbnails in ReactImageGallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -28,6 +28,48 @@ export default function Gallery(){
         {
             original:"https://ngratesc.sirv.com/armor_sphere/IMG-20250410-WA0006%5B1%5D.jpg"
         },
+            {original: "https://amorsphere.sirv.com/20240417_140708.jpg"},
+            {original: "https://amorsphere.sirv.com/20240417_152031.jpg"},
+            {original: "https://amorsphere.sirv.com/20240417_151508.jpg"},
+            {original: "https://amorsphere.sirv.com/20240712_102117.jpg"},
+            {original: "https://amorsphere.sirv.com/20230829_155729(0).jpg"},
+            {original: "https://amorsphere.sirv.com/20231030_160328.jpg"},
+            {original: "https://amorsphere.sirv.com/20240417_150356.jpg"},
+            {original: "https://amorsphere.sirv.com/20240510_094110.jpg"},
+            {original: "https://amorsphere.sirv.com/20240417_151817.jpg"},
+            {original: "https://amorsphere.sirv.com/20240710_121050.jpg"},
+            {original: "https://amorsphere.sirv.com/20250330_114855.jpg"},
+            {original: "https://amorsphere.sirv.com/Screenshot_20240914_215121_WhatsApp.jpg"},
+            {original: "https://amorsphere.sirv.com/20250318_085039.jpg"},
+            {original: "https://amorsphere.sirv.com/20250318_085057.jpg"},
+            {original: "https://amorsphere.sirv.com/20250318_085022.jpg"},
+            {original: "https://amorsphere.sirv.com/20250318_085001.jpg"},
+            {original: "https://amorsphere.sirv.com/20250128_145216.jpg"},
+            {original: "https://amorsphere.sirv.com/20250311_171729.jpg"},
+            {original: "https://amorsphere.sirv.com/20250318_084815.jpg"},
+            {original: "https://amorsphere.sirv.com/20250109_180717.jpg"},
+            {original: "https://amorsphere.sirv.com/20241221_162536.jpg"},
+            {original: "https://amorsphere.sirv.com/20250109_180653.jpg"},
+            {original: "https://amorsphere.sirv.com/20241221_162450.jpg"},
+            {original: "https://amorsphere.sirv.com/20241221_161811.jpg"},
+            {original: "https://amorsphere.sirv.com/20241118_145745.jpg"},
+            {original: "https://amorsphere.sirv.com/20241108_135148.jpg"},
+            {original: "https://amorsphere.sirv.com/20241118_145705.jpg"},
+            {original: "https://amorsphere.sirv.com/20241115_080517.jpg"},
+            {original: "https://amorsphere.sirv.com/20241118_092548.jpg"},
+            {original: "https://amorsphere.sirv.com/20241105_104458.jpg"},
+            {original: "https://amorsphere.sirv.com/20241105_103832.jpg"},
+            {original: "https://amorsphere.sirv.com/20241104_170440.jpg"},
+            {original: "https://amorsphere.sirv.com/20241022_154415.jpg"},
+            {original: "https://amorsphere.sirv.com/20241018_151451.jpg"},
+            {original: "https://amorsphere.sirv.com/20240902_122932.jpg"},
+            {original: "https://amorsphere.sirv.com/20240902_122843.jpg"},
+            {original: "https://amorsphere.sirv.com/20240827_104621.jpg"},
+            {original: "https://amorsphere.sirv.com/20241022_154409.jpg"},
+            {original: "https://amorsphere.sirv.com/20240730_155646.jpg"},
+            {original: "https://amorsphere.sirv.com/20240712_102156.jpg"}
+          
+          
        
     ]
     return(
@@ -37,7 +79,7 @@ export default function Gallery(){
             <p className="text-center mt-4">Explore our range of products and their features.</p>
             </div>
             <div className="container mx-auto mt-10 mb-10">
-                <ReactImageGallery items={images}/>
+                <ReactImageGallery showThumbnails={true} items={images}/>
             </div>
           
         </div>
